a07bf5332e324def131c8feaf17a705f75b34a1c
Declan Kelly <dkelly.home@gmail.com>
2020-09-13 15:13:22 -0700

    Fix redirect page

--- scripts/prep_documentation.sh
+++ scripts/prep_documentation.sh
@@ -8,10 +8,10 @@ DOC_FOLDER="target/doc"
 cat > "$DOC_FOLDER/index.html" <<EOF
 <html>
     <head>
-        <noscript><meta http-equiv="refresh" content="0; url=reckoner/index.html"></noscript>
+        <noscript><meta http-equiv="refresh" content="0; url=yali/index.html"></noscript>
     </head>
-    <body onload="window.location = 'reckoner/index.html'">
-        <a href="reckoner/index.html">Redirect</a>
+    <body onload="window.location = 'yali/index.html'">
+        <a href="yali/index.html">Redirect</a>
     </body>
 </html>
 EOF
