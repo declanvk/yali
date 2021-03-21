bfddf8d73aed3e9f90d1fe1339b1ba7ed9f20318
Declan Kelly <dkelly.home@gmail.com>
2021-03-20 20:37:53 -0700

    Add option to compile binary to execute code

--- cli/src/bin/compile.rs
+++ cli/src/bin/compile.rs
@@ -1,9 +1,14 @@
 use argh::FromArgs;
 use compiler::compile;
+use io::Write;
 use std::{fs, io, path::Path};
-use walox::{compiler, scanner::Scanner, vm::Heap};
+use walox::{
+    compiler,
+    scanner::Scanner,
+    vm::{Heap, VM},
+};
 
-#[derive(FromArgs)]
+#[derive(FromArgs, Debug)]
 /// Compile lox code in a single pass
 struct Args {
     /// script path
@@ -13,21 +18,27 @@ struct Args {
     /// output nicely formatted chunks
     #[argh(switch, short = 'd')]
     dump: bool,
+
+    /// interpret the compiled bytecode
+    #[argh(switch, short = 'r')]
+    intepret: bool,
 }
 
 fn main() {
     tracing_subscriber::fmt::init();
     let args: Args = argh::from_env();
 
-    if run(&args.file_path, args.dump) {
+    tracing::debug!(?args, "Running 'compile' command...");
+    if run(&args.file_path, args.dump, args.intepret) {
         panic!(
             "Encounter errors while running [{}].",
             <String as AsRef<Path>>::as_ref(&args.file_path).display()
         )
     }
+    tracing::debug!(?args, "Ran 'compile' command.");
 }
 
-fn run(file_path: impl AsRef<Path>, should_dump: bool) -> bool {
+fn run(file_path: impl AsRef<Path>, should_dump: bool, should_interpret: bool) -> bool {
     let file_path = file_path.as_ref();
     assert!(
         file_path.exists(),
@@ -42,7 +53,7 @@ fn run(file_path: impl AsRef<Path>, should_dump: bool) -> bool {
 
     let file_contents = fs::read_to_string(file_path).expect("Failed to read file");
     let scanner = Scanner::new(file_contents.as_str());
-    let heap = Heap::new();
+    let mut heap = Heap::new();
     let chunk = match compile(scanner, &heap) {
         Ok(c) => c,
         Err(e) => {
@@ -64,6 +75,21 @@ fn run(file_path: impl AsRef<Path>, should_dump: bool) -> bool {
         chunk
             .write_disassembled(&mut stdout, Some(chunk_name.as_ref()))
             .expect("unable to output chunk");
+    }
+
+    if should_interpret {
+        let mut stdout = io::stdout();
+        let mut vm = VM::new(&mut stdout, chunk, &mut heap);
+        let interp_result = vm.interpret();
+        stdout.flush().expect("Unable to flush stdout");
+
+        match interp_result {
+            Ok(()) => {},
+            Err(err) => {
+                tracing::error!(%err);
+                return true;
+            },
+        }
     }
 
     false
